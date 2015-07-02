85420d86da232866f14c8d72e8c7afa533182b3b
Erez Bosch <erezaka@gmail.com>
2015-07-02 12:22:44 -0700

    complete move sequences

--- game.rb
+++ game.rb
@@ -1,1 +1,17 @@
 require_relative 'board'
+
+class Game
+  def initialize
+    @players = [HumanPlayer.new, HumanPlayer.new]
+    @colors = { @players[0] => :black, @players[1] => :red }
+    @board = Board.prepare_board
+  end
+
+  def play
+    until over?
+      
+    end
+  end
+end
+
+class HumanPlayer
